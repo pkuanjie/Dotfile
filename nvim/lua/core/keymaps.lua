5171abd91f5c4ca605ba8491bb73ba764d660397
pkuanjie <pkuanjie@gmail.com>
2023-03-31 11:44:01 -0400

    up

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = " "
 -- quick quit and esc
 vim.keymap.set("n", "<leader>q", ":x<CR>", opts)
 vim.keymap.set("n", "<leader>w", ":w<CR>", opts)
-vim.keymap.set({ "i", "v" }, "jk", "<ESC>", opts)
+vim.keymap.set({ "i", "v" }, "kj", "<ESC>", opts)
 
 -- buffer navigation
 vim.keymap.set("n", "<leader>bn", ":bnext<CR>", opts)
