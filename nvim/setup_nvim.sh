9d9123677b0ab4770f0d26a07e6e5857bd48fb1d
pkuanjie <pkuanjie@gmail.com>
2024-02-27 13:44:51 -0500

    add setup scripts

---
+++ nvim/setup_nvim.sh
@@ -0,0 +1,9 @@
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+mkdir -p ~/.local/bin/
+mv nvim.appimage ~/.local/bin/
+ln -s ~/.local/bin/nvim.appimage ~/.local/bin/nvim
+source ~/.zshrc
+mkdir -p ~/.config/
+ln -s ~/Dotfiles/nvim ~/.config/nvim
+echo "Neovim installed and configured"
---
+++ setup_zsh_tmux_nvim_miniconda.sh
@@ -0,0 +1,5 @@
+bash ~/Dotfile/zsh/setup_zsh.sh
+bash ~/Dotfile/tmux/setup_tmux.sh
+bash ~/Dotfile/miniconda/setup_miniconda.sh
+bash ~/Dotfile/nvim/setup_nvim.sh
+echo "Done"
