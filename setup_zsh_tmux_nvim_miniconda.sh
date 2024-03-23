89d0672ae7bac0077bd560dd7893869c5cef8411
pkuanjie <pkuanjie@gmail.com>
2024-03-23 12:32:08 -0400

    up

--- setup_zsh_tmux_nvim_miniconda.sh
+++ setup_zsh_tmux_nvim_miniconda.sh
@@ -2,4 +2,5 @@ bash ~/Dotfile/zsh/setup_zsh.sh
 bash ~/Dotfile/tmux/setup_tmux.sh
 bash ~/Dotfile/miniconda/setup_miniconda.sh
 bash ~/Dotfile/nvim/setup_nvim.sh
+ln -s ~/Dotfile/tmux/.tmux.conf ~/.tmux.conf
 echo "Done"
