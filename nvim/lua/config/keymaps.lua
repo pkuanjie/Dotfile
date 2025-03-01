6f6598bda0aae2655471bea8e2a08c0622c1191b
Jie An Apple Mac <pkuanjie@gmail.com>
2025-03-01 15:28:38 -0500

    remove m-j

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,9 @@
 -- Add any additional keymaps here
 local map = LazyVim.safe_keymap_set
 map("i", "kj", "<ESC>")
+vim.api.nvim_del_keymap("n", "<M-k>")
+vim.api.nvim_del_keymap("n", "<M-j>")
+vim.api.nvim_del_keymap("v", "<M-k>")
+vim.api.nvim_del_keymap("v", "<M-j>")
+vim.api.nvim_del_keymap("i", "<M-k>")
+vim.api.nvim_del_keymap("i", "<M-j>")
