0b7c95d2cef2ba978bcfe855c0bd954eb8d469c2
Jie An <pkuanjie@gmail.com>
2026-02-08 23:04:00 -0800

    up treesitter

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -67,10 +67,8 @@ require("lazy").setup({
 	-- Treesitter & text objects
 	{
 		"nvim-treesitter/nvim-treesitter",
-		build = function()
-			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-			ts_update()
-		end,
+		lazy = false,
+		build = ":TSUpdate",
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
